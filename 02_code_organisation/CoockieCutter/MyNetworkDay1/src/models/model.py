2c5191ba25360a4866047b21c0d0a61c01b8ef37
Fredferik Kjaer <f.kjaer@icloud.com>
2021-06-09 12:10:52 +0200

    Better network

--- 02_code_organisation/CoockieCutter/MyNetworkDay1/src/models/model.py
+++ 02_code_organisation/CoockieCutter/MyNetworkDay1/src/models/model.py
@@ -1,11 +1,19 @@
 import torch.nn.functional as F
+import torch
 from torch import nn, optim
 
+kernel_size = 5
+padding = 2
+channel_sizes = [1,6,16]
 
 class MyAwesomeModel(nn.Module):
     def __init__(self):
         #Der mangler convolution layer
         super().__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels = channel_sizes[0], out_channels = channel_sizes[1], kernel_size = kernel_size, padding = padding)
+        self.conv2 = nn.Conv2d(
+            in_channels = channel_sizes[1], out_channels = channel_sizes[2], kernel_size = kernel_size, padding = padding)
         self.fc1 = nn.Linear(784, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, 64)
@@ -16,9 +24,16 @@ class MyAwesomeModel(nn.Module):
 
     def forward(self, x):
         # make sure input tensor is flattened
-        x = x.view(x.shape[0], -1)
-
+        if x.ndim != 4:
+            raise ValueError('Expected input to a 4D tensor but instead it is: ', x.ndim)
+        if x.shape[1] != 1 or x.shape[2] != 28 or x.shape[3] != 28:
+            raise ValueError('Expected each sample to have shape [1, 28, 28] but had: ', x.shape )
         # Now with dropout
+        x = self.dropout(F.max_pool2d(F.relu(self.conv1(x)),(2,2)))
+        x = self.dropout(F.max_pool2d(F.relu(self.conv2(x)),(2,2)))
+
+        x = torch.flatten(x,1)
+
         x = self.dropout(F.relu(self.fc1(x)))
         x = self.dropout(F.relu(self.fc2(x)))
         x = self.dropout(F.relu(self.fc3(x)))
