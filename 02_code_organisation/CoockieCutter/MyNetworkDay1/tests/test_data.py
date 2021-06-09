08440902a85006e74c57fc90db02b6ae955dbd23
Fredferik Kjaer <f.kjaer@icloud.com>
2021-06-09 13:15:08 +0200

    testing

--- 02_code_organisation/CoockieCutter/MyNetworkDay1/tests/test_data.py
+++ 02_code_organisation/CoockieCutter/MyNetworkDay1/tests/test_data.py
@@ -1,21 +1,20 @@
-from torchvision import datasets, transforms
 import torch
 import numpy as np
+import pytest
 #import os
 #print(os.listdir("../data/processed/training.pt"))
-train_data, train_label = torch.load('../data/processed/training.pt')
+train_data, train_label = torch.load('/Users/frederikkjaer/Documents/DTU/8Semester/MLOps/dtu_mlops/02_code_organisation/CoockieCutter/MyNetworkDay1/data/processed/training.pt')
 train_data = torch.unsqueeze(train_data,1)
 #trainset = torch.utils.data.TensorDataset(*trainset)
-test_data, test_label = torch.load('../data/processed/test.pt')
+test_data, test_label = torch.load('/Users/frederikkjaer/Documents/DTU/8Semester/MLOps/dtu_mlops/02_code_organisation/CoockieCutter/MyNetworkDay1/data/processed/test.pt')
 test_data = torch.unsqueeze(test_data,1)
+
 #testset = torch.utils.data.TensorDataset(*testset)
-def test_data(train_data = train_data, train_label = train_label, test_data= test_data, test_label = test_label):
-    assert len(train_data) == 60000 
-    assert len(test_data) == 10000
-    assert next(iter(train_data)).shape == torch.Size([1,28,28])
-    assert next(iter(test_data)).shape == torch.Size([1,28,28])
-    assert np.array_equal(np.unique(train_label), np.arange(10))
-    assert np.array_equal(np.unique(test_label), np.arange(10))
-    #assert that each datapoint has shape [1,28,28] or [728] depending on how you choose to format
-    #assert that all labels are represented
-test_data()
+@pytest.mark.parametrize("test_input,expected", [("train_data", torch.Size([1,28,28])), ("test_data", torch.Size([1,28,28]))])
+def test_data_shape(test_input, expected):
+    assert eval(test_input)[0].shape == expected
+
+
+@pytest.mark.parametrize("test_input,expected", [("train_label", np.arange(10)), ("test_label", np.arange(10))])
+def test_label_values(test_input, expected):
+    assert np.array_equal(np.unique(eval(test_input)), expected)
--- 02_code_organisation/CoockieCutter/MyNetworkDay1/tests/test_model.py
+++ 02_code_organisation/CoockieCutter/MyNetworkDay1/tests/test_model.py
@@ -1,16 +1,17 @@
 import sys
 import torch
-sys.path.insert(1,'../src/models/')
+import pytest
+sys.path.insert(1,'/Users/frederikkjaer/Documents/DTU/8Semester/MLOps/dtu_mlops/02_code_organisation/CoockieCutter/MyNetworkDay1/src/models')
 from model import MyAwesomeModel
 model = MyAwesomeModel()
 batch_size = 64
 
-train_data, train_label = torch.load('../data/processed/training.pt')
+train_data, train_label = torch.load('/Users/frederikkjaer/Documents/DTU/8Semester/MLOps/dtu_mlops/02_code_organisation/CoockieCutter/MyNetworkDay1/data/processed/training.pt')
 train_data_unsqueezed = torch.unsqueeze(train_data,1)
 trainloader = torch.utils.data.DataLoader(
     torch.utils.data.TensorDataset(*(train_data_unsqueezed,train_label)), batch_size=batch_size, shuffle=True)
 
-test_data, test_label = torch.load('../data/processed/test.pt')
+test_data, test_label = torch.load('/Users/frederikkjaer/Documents/DTU/8Semester/MLOps/dtu_mlops/02_code_organisation/CoockieCutter/MyNetworkDay1/data/processed/test.pt')
 test_data_unsqueezed = torch.unsqueeze(test_data,1)
 testloader = torch.utils.data.DataLoader(
     torch.utils.data.TensorDataset(*(test_data_unsqueezed,test_label)), batch_size=batch_size, shuffle=True)
@@ -21,4 +22,12 @@ def test_model(trainloader = trainloader):
         assert output.shape == torch.Size([batch_size, 10])
         break
 
-test_model()
+def test_raises_warnings():
+    model = MyAwesomeModel()
+    batch_size = 64
+    with pytest.raises(ValueError):
+        test1 = torch.randn(1,28,28)
+        output = model(test1)
+    with pytest.raises(ValueError):
+        test2 = torch.randn(batch_size,3,28,28)
+        output = model(test2)
